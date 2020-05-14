db7fa4a35f6ff2ab0bb7340d4b3c028831d38eee
Max <48124450+maxpertici@users.noreply.github.com>
2020-05-14 08:39:38 +0200

    init

---
+++ README.md
@@ -0,0 +1,60 @@
+
+# WP Block Revealer
+License : GPLv2
+License URI : http://www.gnu.org/licenses/gpl-2.0.html
+
+## Description
+Hightlight your blocks and stop clicking everywhere to find them.
+
+
+## Installation
+1. Install the plugin and activate.
+2. Use shortcut : Ctrl + Alt + R
+
+## Screenshots
+
+Result with simple blocks
+![Result with simple blocks](/.wordpress.org/screenshot-1.png?raw=true "Result with simple blocks")
+
+Result with columns block
+![Result with columns block](/.wordpress.org/screenshot-2.png?raw=true "Result with columns blocks")
+
+Result with group block
+![Result with columns block](/.wordpress.org/screenshot-3.png?raw=true "Result with group blocks")
+
+## Changelog
+
+### 0.9.0
+* Larger breadcrumb font-size
+* Clean UI
+* Reveal inster point
+* Fix column hover and selected states
+
+### 0.8.0
+* improve blocks support (soon widget and embed)
+* remove unused files
+* version number added to asset url
+
+### 0.7.0
+* group support
+* add screenshot 3
+
+### 0.6.0
+* screenshots replacement
+
+### 0.5.0
+* readme fix
+* screenshots replacement
+
+### 0.4.0
+* Preparation for wp.org
+
+### 0.3.0
+* Improve read.me
+
+### 0.2.0
+* Improve highlights for columns
+* Load assets only for gutenberg editor
+
+### 0.1.0
+* The idea
---
+++ wp-quick-main-menu.php
@@ -0,0 +1,62 @@
+<?php
+/*
+Plugin Name: Quick Main Menu for Gutenberg
+Plugin URI: https://m.pertici.fr
+Description: Get main menu when editing post with gutenberg fullscreen
+Author: Maxime Pertici
+Version: 0.1
+Author URI: https://m.pertici.fr
+Contributors:
+License:      GPLv2
+License URI:  https://www.gnu.org/licenses/gpl-2.0.html
+Text Domain:  wp-qmm
+Domain Path:  /languages
+Copyright 2020 WP Quick Menu
+*/
+
+defined( 'ABSPATH' ) or	die();
+
+
+function wp_qmm_script(){
+
+	// test + toogle css class
+	$script =
+		"jQuery( window ).load(function() {
+
+			const isFullscreenMode = wp.data.select( 'core/edit-post' ).isFeatureActive( 'fullscreenMode' );
+			
+			// if ( isFullscreenMode ) { wp.data.dispatch( 'core/edit-post' ).toggleFeature( 'fullscreenMode' ); }
+			
+			if ( isFullscreenMode ) {
+
+				jQuery('body').addClass('wp-qmm-enable');
+
+				jQuery('.edit-post-header .components-button.edit-post-fullscreen-mode-close').hoverIntent(
+					function(){ jQuery('body').addClass('wp-qmm-open'); console.log('open'); }, 100,
+					function(){ jQuery('body').removeClass('wp-qmm-open'); console.log('close'); }
+				);
+
+				jQuery('#adminmenumain').hover(
+					function(){ jQuery('body').addClass('wp-qmm-open'); },
+					function(){ jQuery('body').removeClass('wp-qmm-open'); }
+				);
+
+			}
+
+						
+		});";
+
+		wp_enqueue_script( 'hoverIntent' );
+		wp_add_inline_script( 'wp-blocks', $script );
+	}
+
+	add_action( 'enqueue_block_editor_assets', 'wp_qmm_script' );
+
+
+function wp_qmm_admin_scripts( $hook ) {
+
+    wp_enqueue_style( 'gut-qmmm-style', plugin_dir_url( __FILE__ ) . '/wp-quick-main-menu.css', array(), false, 'all' );
+}
+
+// enqueue styles
+add_action( 'admin_enqueue_scripts', 'wp_qmm_admin_scripts' );
